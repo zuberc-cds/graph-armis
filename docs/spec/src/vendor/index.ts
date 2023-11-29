21e516bbda8ab38189b75beecb0910e453d0eb86
nirav-crest <nirav.kelaiya@crestdatasys.com>
2023-11-29 16:20:18 +0530

    Adding vendor spec

---
+++ docs/spec/src/vendor/index.ts
@@ -0,0 +1,20 @@
+import { StepSpec, RelationshipClass } from '@jupiterone/integration-sdk-core';
+import { IntegrationConfig } from '../../../../src/config';
+
+export const accountSpec: StepSpec<IntegrationConfig>[] = [
+  {
+    id: 'fetch-vendor',
+    name: 'Armis',
+    category: ['security'],
+    entities: [
+      {
+        resourceName: 'Vendor',
+        _type: 'armis_vendor',
+        _class: ['Vendor'],
+      },
+    ],
+    relationships: [],
+    dependsOn: [],
+    implemented: true,
+  },
+];
