1c3c960d0f7eaddf395453489b709b7fde08a4b4
nirav-crest <nirav.kelaiya@crestdatasys.com>
2023-11-29 16:21:52 +0530

    Removing unused imports

--- docs/spec/src/vendor/index.ts
+++ docs/spec/src/vendor/index.ts
@@ -1,4 +1,4 @@
-import { StepSpec, RelationshipClass } from '@jupiterone/integration-sdk-core';
+import { StepSpec } from '@jupiterone/integration-sdk-core';
 import { IntegrationConfig } from '../../../../src/config';
 
 export const accountSpec: StepSpec<IntegrationConfig>[] = [
