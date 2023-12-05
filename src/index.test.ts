d87ca4b30cab75fbbd2e0d9a499eaf6117f5a8ef
Zuber Chataiwala <zuber.chataiwala@crestdatasys.com>
2023-12-05 16:33:58 +0530

    Adding retry and pagination

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
-import { invocationConfig as implementedConfig } from '.';
-import { invocationConfig as specConfig } from '../docs/spec/src';
+// import { invocationConfig as implementedConfig } from '.';
+// import { invocationConfig as specConfig } from '../docs/spec/src';
 
 // test('implemented integration should match spec', () => {
 //   expect(implementedConfig).toImplementSpec(specConfig);
